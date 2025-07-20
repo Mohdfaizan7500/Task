ffbc6ec145f4eec607e40c7c7583ba0f8319fd4d
Mohdfaizan7500 <mohdfaizankhan7500@gmail.com>
2025-07-20 11:28:01 -0700

    update

--- App.jsx
+++ App.jsx
@@ -1,11 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import TaskManagerContainer from './TaskManager/navigation/TaskManagerContainer'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+      <TaskManagerContainer/>
   )
 }
 
--- index.js
+++ index.js
@@ -5,12 +5,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-// import Main from './src/screens/Main';
-// import Login from './src/screens/Login';
-// import Splash from './src/screens/Splash';
-// import Home from './src/screens/Home';
-// import Todo from './scr2/Todo';
-// import TodoAppContainer from './todo/TodoApp/Navigation';
-import TaskManagerContainer from './TaskManager/navigation/TaskManagerContainer';
 
-AppRegistry.registerComponent(appName, () => TaskManagerContainer);
+
+AppRegistry.registerComponent(appName, () => App);
